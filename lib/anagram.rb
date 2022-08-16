a05c98c2a7a032eabacbfd41f44d0d3fa3f93896
TONNYCHWEYA <chweyatonny@gmail.com>
2022-08-16 15:46:50 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        matched_word = []
+        array_of_char = @word.chars
+        matched_word = array.filter do |word|
+        word.chars.sort == array_of_char.sort
+     end
+    end
+end
